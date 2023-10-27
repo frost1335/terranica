b858f4bfefa13cf4056a1c5ab152601309918090
frost1335 <dilrozbek.developer@gmail.com>
2023-10-27 22:01:26 +0500

    add: 3d model

---
+++ js/script.js
@@ -0,0 +1,84 @@
+//Import the THREE.js library
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+// To allow for the camera to move around the scene
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+// To allow for importing the .gltf file
+import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+
+const container = document.getElementById("threejs-container");
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+let mouseX = window.innerWidth / 2;
+let mouseY = window.innerHeight / 2;
+
+let object;
+
+let controls;
+
+let objToRender = "car";
+
+const loader = new GLTFLoader();
+
+loader.load(
+  `../models/car/scene.gltf`,
+  function (gltf) {
+    object = gltf.scene;
+    scene.add(object);
+  },
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  function (error) {
+    console.error(error);
+  }
+);
+
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+
+// size of render scene
+renderer.setSize(container.clientWidth, container.clientHeight);
+
+container.appendChild(renderer.domElement);
+
+camera.position.z = objToRender === "car" ? 200 : 500;
+
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(500, 500, 500);
+topLight.castShadow = true;
+scene.add(topLight);
+
+const ambientLight = new THREE.AmbientLight(0x333333, 5);
+scene.add(ambientLight);
+
+controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  if (object && objToRender === "car") {
+    //I've played with the constants here until it looked good
+    object.rotation.y = -3 + (mouseX / window.innerWidth) * 3;
+    object.rotation.x = -1.2 + (mouseY * 2.5) / window.innerHeight;
+  }
+  renderer.render(scene, camera);
+}
+
+window.addEventListener("resize", (e) => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+document.onmousemove = (e) => {
+  mouseX = e.clientX;
+  mouseY = e.clientY;
+};
+
+animate();
